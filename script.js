c506bd66950301fb0218b81aeebbb27d6c59eee6
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-04-03 15:51:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+const array = [1, 2, 3, 4];
+
+function filterEven() {
+  return new Promise((resolve, reject) => {
+    const evenArray = array.filter((e) => e % 2 == 0);
+    setTimeout(() => {
+      output.innerText = `${evenArray}`;
+      resolve(evenArray);
+    }, 1000);
+  });
+}
+
+function multiplyByTwo(val) {
+  return new Promise((resolve, reject) => {
+    const result = val.map((e) => e * 2);
+    setTimeout(() => {
+      output.innerText = `${result}`;
+      resolve(result);
+    }, 2000);
+  });
+}
+
+filterEven().then((val) => multiplyByTwo(val));
